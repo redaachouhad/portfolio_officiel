16b3112c7df6a1d86f144aad5de7ed9e2d80f1a0
redaachouhad <redachouhad@gmail.com>
2024-03-12 23:50:15 +0000

    menu animation duration

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -17,7 +17,7 @@ function NavBar() {
     }
   };
   return (
-    <div className="fixed left-0 top-0 w-full text-white bg-[rgb(35,26,83)] px-4 sm:px-6 md:px-10 py-2 flex justify-between items-center z-50">
+    <div className="fixed left-0 top-0 w-full text-white bg-[rgb(61,44,156)] px-4 sm:px-6 md:px-10 py-2 flex justify-between items-center z-50">
       <div className="flex items-center justify-center w-fit gap-2">
         <PiFlowerLotusLight className="text-2xl md:text-3xl font-semibold" />
         <span className="text-lg md:text-2xl font-semibold">Portfolio</span>
@@ -30,7 +30,7 @@ function NavBar() {
       </div>
 
       <div
-        className={`absolute lg:static top-full lg:top-auto left-0 lg:left-auto w-full lg:w-fit overflow-hidden transition-height duration-500 ease ${heightMenu} lg:h-fit`}
+        className={`absolute lg:static top-full lg:top-auto left-0 lg:left-auto w-full lg:w-fit overflow-hidden transition-height duration-100 ease ${heightMenu} lg:h-fit`}
       >
         <div className="flex lg:flex-row flex-col gap-10 py-6 lg:py-0 w-full lg:w-fit items-center bg-[rgba(0,0,0,0.5)] lg:bg-transparent backdrop-blur-xl">
           {itemMenu.map((item, index) => {
